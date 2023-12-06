dbd6f2066894c2d2f243cedc3aee259a60b02f48
Henry Lau <henrylauforwork@gmail.com>
2023-12-06 16:54:21 +0800

    v2

--- js/global.js
+++ js/global.js
@@ -1,3 +1,61 @@
-const tabIcon = '<link href="img/logo.png" rel="icon">';
-document.head.insertAdjacentHTML('beforeend', tabIcon);
+//sub page only
+document.addEventListener("DOMContentLoaded", function () {
+    const tabIcon = '<link href="../img/logo.png" rel="icon">';
+    document.head.insertAdjacentHTML('beforeend', tabIcon);
 
+    const navbarHTML = `
+    <div class="container-fluid bg-white sticky-top">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg bg-white navbar-light py-2 py-lg-0">
+                <a href="../index.html" class="navbar-brand">
+                    <img class="img-fluid" src="../img/logo.png" alt="Logo">
+                </a>
+                <button type="button" class="navbar-toggler ms-auto me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto">
+                        <a href="../index.html" id="home" class="nav-item nav-link">Home</a>
+                        <a href="./about.html" id="about" class="nav-item nav-link">About Us</a>
+                        <a href="./news.html" id="news" class="nav-item nav-link">News</a>
+                        <a href="./explore.html" id="explore" class="nav-item nav-link">Explore</a>
+                        <a href="./analysis.html" id="analysis" class="nav-item nav-link">Analysis</a>
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">More</a>
+                            <div class="dropdown-menu bg-light rounded-0 m-0">
+                                <a href="./coming-soon.html" class="dropdown-item">Coming Soon</a>
+                            </div>
+                        </div>
+                        <a href="./contact.html" id="contact" class="nav-item nav-link">Contact</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    `;
+    document.getElementById("navbar-container").innerHTML = navbarHTML;
+
+    const footerHTML = `<div class="container-fluid copyright py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                &copy; <a class="fw-medium" href="#">Tea House</a>, All Right Reserved.
+            </div>
+        </div>
+    </div>
+</div>`
+    document.getElementById("footer-container").innerHTML = footerHTML;
+    navHover();
+});
+
+function navHover() {
+    const page = ["about", "news", "explore", "contact"];
+    for (let i = 0; i < page.length; i++) {
+        let element = document.getElementById(page[i]);
+        element.classList.remove("active");
+    }
+    const currentPage = window.location.pathname.split('/').pop().split('.')[0];
+    const activeLink = document.getElementById(currentPage.toLowerCase());
+    // console.log(activeLink);
+    activeLink.classList.add("active");
+}
---
+++ js/newsDetails.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+
+    newsDetails(id);
+});
+
+async function newsDetails(id) {
+    try {
+        // const response = await fetch('./newsDetails.json');
+        const response = await fetch('../js/newsDetails.json');
+        const details = await response.json();
+
+        const detail = details.find((item) => item.id == id);
+
+        if (detail) {
+            const detailsHTML = generateNewsDetailsHTML(detail);
+            document.getElementById("newsDetails").innerHTML = detailsHTML;
+        } else {
+            const comingSoon = comingSoonHTML();
+            document.getElementById("error").innerHTML = comingSoon;
+            console.error('Details not found for id:', id);
+        }
+    } catch (error) {
+        console.error('Error fetching details:', error);
+    }
+}
+
+function generateNewsDetailsHTML(detail) {
+    return `
+        <div class="container-fluid product py-5">
+            <div class="container">
+                <div class="row g-5">
+                    <div class="col-lg-5 wow fadeIn" data-wow-delay="0.1s">
+                        <img class="img-fluid" src="${detail.img}" alt="photo">
+                    </div>
+                    <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                        <div class="section-title">
+                            <p class="fs-5 fw-medium fst-italic text-primary">${detail.title}</p>
+                        </div>
+                        <p class="mb-4">${detail.content}</p>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+}
+
+function comingSoonHTML() {
+    return `
+    <div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container text-center">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+            <p class="fs-1 fw-medium fst-italic text-primary">Coming Soon</p>
+                <div class="coming">
+                    <img src="../img/comingSoon.jpg" alt="Coming Soon">
+                </div>
+                <h1 id="stayTuned" class="mb-4" style="color: #88b44e;"></h1>
+                <a class="btn btn-primary rounded-pill py-3 px-5" href="../index.html">Go Back To Home</a>
+            </div>
+        </div>
+    </div>
+</div>`
+}
